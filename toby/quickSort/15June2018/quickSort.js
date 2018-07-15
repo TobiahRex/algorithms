69dea9537121ff65e1756f919fa0d2464414e5e2
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-15 13:18:28 -0700

    building quicksort.js.

--- toby/quickSort/15June2018/quickSort.js
+++ toby/quickSort/15June2018/quickSort.js
@@ -9,5 +9,23 @@ function quickSort(arr, lo = 0, hi = (arr.length - 1)) {
   if (lo > hi || lo === hi) return;
 
   let p = partition(arr, lo, hi);
-  quickSort(arr, )
+  quickSort(arr, lo, p - 1);
+  quickSort(arr, p + 1, hi);
+}
+
+function partition(arr, lo, hi) {
+  let iNext = lo,
+    iPartition = hi;
+
+  while(true) {
+    if (arr[iNext] > arr[iPartition]) {
+
+    }
+  }
+}
+
+function swap(arr, i1, i2) {
+  const temp = arr[i1],
+    arr[i1] = arr[i2],
+    arr[i2]= temp;
 }
