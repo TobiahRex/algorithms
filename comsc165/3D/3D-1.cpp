5edec3d18156749d7f60c0331f97da7d46a096ca
Tobiah Rex <tobiah.rex@gmail.com>
2018-10-04 23:59:38 -0700

    building out 3D | Binary Search logic - Linear Search = done.

--- comsc165/3D/3D-1.cpp
+++ comsc165/3D/3D-1.cpp
@@ -1,60 +1,83 @@
 #include <iostream>
 #include <vector>
-#include <limit>
 using namespace std;
 
 int LinearSearch(vector<int>, int);
-// int BinarySearch(vector<int>, int);
+int BinarySearch(vector<int>, int);
 
 int main() {
   vector<int> nums = {101, 142, 147, 189, 199, 207, 222, 234, 289, 296, 310, 319, 388, 394, 417, 429, 447, 521, 536, 600};
-
-  cout << "Enter value to search for: ";
   int target{};
-  cin >> target;
 
   bool invalid{true};
   while(invalid) {
+    cout << "Enter target value to search for: ";
+    cin >> target;
     if (target < 0 || cin.fail() || cin.bad()) {
-      cout << "Target value must be greater than 0";
+      cout << "\n\t\t ERROR \nTarget value must be greater than 0\n" << endl;
       cin.clear();
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    } else {
+      invalid = false;
     }
   }
 
   int linear_comparisons = LinearSearch(nums, target);
-  // int binary_comparisons = LinearSearch(nums, findNum);
+  cout << "Comparisons: " << linear_comparisons << endl;
 
-  cout << "\t\t -- Linear Search Results -- " << endl
-  << "Comparisons: " << linear_comparisons << endl;
-  // << "\t\t -- Binary Search Results -- " << endl
-  // << "Comparisons: " << binary_comparisons << endl;
+  int binary_comparisons = BinarySearch(nums, target);
+  cout << "Comparisons: " << binary_comparisons << endl;
 
   return 0;
 }
 
 int LinearSearch(vector<int> nums, int target) {
-  int comparisons{}, position{}, lastVal{}, size = nums.size();
+  cout << "\n\t -- Linear Search Results -- " << endl;
+  int comparisons{}, lastVal{}, i{0}, size = nums.size();
 
-  for (int i = 0; nums[i] != target; i++) {
+  for (;lastVal != target && i < size; i++) {
+    // cout << i << " ";
     comparisons += 1;
-    position = i;
     lastVal = nums[i];
   }
 
   bool found{false};
-  if (nums[i] == target) found = true;
-
+  if (lastVal == target) found = true;
 
   found == true ?
-    cout << "Linear Search found Value at index " << position << endl :
-    cout << "Linear Search did NOT find value" << endl;
+    cout << "\nLinear Search found Value at position " << i + 1 << endl :
+    cout << "\nLinear Search did NOT find value" << endl;
 
   return comparisons;
 }
 
-// int BinarySearch(vector<int> nums, int target) {
-//   int comparisons{}, size = nums.size();
-//
-//
-// }
+int BinarySearch(vector<int> nums, int target) {
+  int comparisons{},
+    first{},
+    last = nums.size() - 1,
+    position{-1},
+    middle{};
+
+  bool found{false};
+
+  while (!found && first <= last) {
+    comparisons += 1;
+    middle = nums[first - last] / 2;
+    cout << middle << endl;
+
+    if (target == nums[middle]) {
+      position = middle;
+      found = true;
+    } else if (target < nums[middle]) {
+      last = middle - 1;
+    } else {
+      first = middle + 1;
+    }
+  }
+
+  found == true ?
+    cout << "\nBinary Search found Value at position " << position << endl :
+    cout << "\nBinary Search did NOT find value" << endl;
+
+  return comparisons;
+}
