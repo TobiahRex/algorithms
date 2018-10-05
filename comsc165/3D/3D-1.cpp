80121b618cdeaae5dba900cae869adbb6e83f8f7
Tobiah Rex <tobiah.rex@gmail.com>
2018-10-04 23:07:28 -0700

    building 3D.

---
+++ comsc165/3D/3D-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int LinearSearch(vector<int>, int);
+int BinarySearch(vector<int>, int);
+
+int main() {
+  vector<int> nums = {101, 142, 147, 189, 199, 207, 222, 234, 289, 296, 310, 319, 388, 394, 417, 429, 447, 521, 536, 600};
+
+  int linear_comparisons = LinearSearch(nums, findNum);
+  int binary_comparisons = LinearSearch(nums, findNum);
+
+  cout << "Li"
+
+  return 0;
+}
