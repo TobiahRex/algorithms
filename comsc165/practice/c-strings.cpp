7f6a0158f40504a730ff3120518606d0c7f35b0f
Tobiah Rex <tobiah.rex@gmail.com>
2018-10-09 20:00:43 -0700

    correctly casting c-strings to interger.

---
+++ comsc165/practice/c-strings.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int iNum;
+  char cstr[] = "123123";
+
+  iNum = atoi(cstr);
+
+  cout << iNum << endl;
+
+
+  return 0;
+}
