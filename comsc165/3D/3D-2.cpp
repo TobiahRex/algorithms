627f512460b4d849d57e9ae9c3dd3b24494eed79
Tobiah Rex <tobiah.rex@gmail.com>
2018-10-11 21:33:29 -0700

    finished building bubble srot.

--- comsc165/3D/3D-2.cpp
+++ comsc165/3D/3D-2.cpp
@@ -48,11 +48,12 @@ int SelectionSort(vector<int> arr) {
 
   for (int i{0}; i < size; i++) {
     for (int j = i + 1; j < size; j++) {
-      if (arr[i] > arr[j]) {
-        exchanges += 1;
+      if (arr[j] < arr[i]) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+
+        exchanges += 1;
       }
     }
   }
