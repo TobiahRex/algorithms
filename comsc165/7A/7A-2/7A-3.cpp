341ce8cbce6bbef771837565d942eca5063e43c3
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-06 20:53:20 -0800

    building out 7A-3.

--- comsc165/7A/7A-2/7A-3.cpp
+++ comsc165/7A/7A-2/7A-3.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+class SavingsAccount {
+  private:
+    int dollars;
+    int cents;
+  public:
+    void _parseDeposit(int &dollars, int &cents) {
+      while(cents >= 100) {
+        dollars += 1;
+        cents -= 100;
+      }
+    }
+    void openAccount(float initDeposit);
+    void makeDeposit(float deposit);
+    void getBalance() const;
+};
+
 int main() {
   return 0;
 }
