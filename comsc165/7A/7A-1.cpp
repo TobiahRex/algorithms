6aab5dd56e6608a361a7ccb755d49ccfa9381378
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-06 16:11:03 -0800

    finished with 7A.

--- comsc165/7A/7A-1.cpp
+++ comsc165/7A/7A-1.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+#include <iomanip>
+#include "Circle.h"
+
+using namespace std;
+
+int main() {
+  Circle c;
+  c.setRadius(5);
+  cout << c.getRadius() << endl;
+  cout << setprecision(2) << fixed << c.getArea() << endl;
+
+  return 0;
+}
