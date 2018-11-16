07feb3ea040100f9053c1fd7cf256762799f2fd6
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-15 20:26:36 -0800

    finished rough-draft of LinkedList->display() for Lab 9.

--- comsc165/9/LinkedList.h
+++ comsc165/9/LinkedList.h
@@ -4,7 +4,7 @@
 class LinkedList {
   private:
     struct ListNode {
-      double value;
+      double data;
       struct ListNode *next;
     };
 
@@ -16,12 +16,12 @@ class LinkedList {
     ~LinkedList();
     LinkedList(const LinkedList &oldList);
 
-    void appendNode(double data) {
+    void appendNode(double d) {
       ListNode *newNode;
       ListNode *nextNode;
 
       newNode = new ListNode;
-      newNode->value = data;
+      newNode->data = d;
       newNode->next = nullptr;
 
       if (head) {
@@ -34,9 +34,25 @@ class LinkedList {
         head = newNode;
       }
     }
-    void insertNode(double value, double location);
-    void deleteNode(double value);
-    void display() const;
+    void insertNode(double data, double location);
+    void deleteNode(double data);
+    int display() const {
+      ListNode *nextNode;
+
+      if (head) {
+        int nodeCount{0};
+        cout << "Head: " << head.data << endl;
+        nextNode = head->next;
+        while(nextNode->next) {
+          ++nodeCount;
+          cout << "Node #" << nodeCount << " : " << nextNode.data << endl;
+          nextNode = nextNode->next;
+        }
+        return 0;
+      }
+      cout << head->data;
+      return 0;
+    };
     void reverseList();
 };
 #endif
