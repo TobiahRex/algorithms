293cba8c2eb3b09d58b67e4e9add2cce10d3fe4d
Tobiah Rex <tobiah.rex@gmail.com>
2018-12-04 18:48:16 -0800

    building out 10B-2.

---
+++ comsc165/10/stacks-queues/10B-2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Queue {
+private:
+  struct QueueNode {
+    float data;
+    struct QueueNode* next;
+  }
+  QueueNode *front;
+  QueueNode *rear;
+  int numItems;
+
+  Queue() {
+    front = nullptr;
+    rear = nullptr;
+    numItems = 0;
+  }
+  ~Queue() {
+    clear();
+  }
+  void enqueue(float f) {
+    QueueNode *newNode = nullptr;
+    newNode = new QueueNode;
+    newNode->data = f;
+    newNode->next = nullptr;
+
+    if (isEmpty()) {
+      front = newNode;
+      rear = newNode;
+    } else {
+      rear->next = newNode;
+      rear = newNode;
+    }
+    numItems += 1;
+  }
+  float dequeue() {
+    QueueNode *temp = nullptr;
+    float returnData;
+    if (isEmpty()) {
+      cout << "Queue is empty." << endl;
+    } else {
+      returnData = front->data;
+      temp = front;
+      front = front->next;
+      delete temp;
+      numItems -= 1;
+    }
+
+    return returnData;
+  }
+  bool isEmpty() const;
+  void clear();
+}
+
+int main() {
+  Queue numsQueue;
+  for (let i = 1; i <= 5; i++) {
+    numsQueue.enqueue(i * 1.1);
+  }
+  while(!numsQueue.isEmpty()) {
+    cout << numsQueue.dequeue() << endl;
+  }
+  return 0;
+}
